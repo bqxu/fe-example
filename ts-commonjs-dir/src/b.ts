ebfbd38cc1b3ece9573837e3785c09a2aaad5bed
bqxu <bqxu.me@gmail.com>
2022-04-19 00:23:22 +0800

    init commit

---
+++ ts-commonjs-dir/src/b.ts
@@ -0,0 +1,3 @@
+export const func_b = () => {
+  console.log("b");
+};
---
+++ ts-system-index/src/index.ts
@@ -0,0 +1,7 @@
+import { func_a } from "./a";
+import { func_b } from "./b";
+
+export const func = () => {
+  func_a();
+  func_b();
+};
